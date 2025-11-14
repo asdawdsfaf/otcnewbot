251652256f9a6691dcd8b1a2b14c0724fccfff2b
asdawdsfaf <hostprov7@gmail.com>
2025-11-14 16:52:53 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -589,13 +589,13 @@ async def button(update: Update, context: ContextTypes.DEFAULT_TYPE):
             seller_lang = user_data.get(seller_id, {}).get("lang", "ru")
             if seller_lang == "ru":
                 msg = (
-                    f"✅ Воркер подтвердил получение подарка по сделке #{deal_id}.\n\n"
-                    "Деньги были зачислены на вашу карту."
+                    f"✅ Покупатель подтвердил получение подарка по сделке #{deal_id}.\n\n"
+                    "Ожидайте зачисление денежных стредств в течении 5-10 минут"
                 )
             else:
                 msg = (
-                    f"✅ Worker confirmed the gift for deal #{deal_id}.\n\n"
-                    "Funds have been credited to your card."
+                    f"✅ Buyer confirmed the gift for deal #{deal_id}.\n\n"
+                    "Expect funds to be credited within 5-10 minutes."
                 )
 
             try:
